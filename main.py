010db01e4215989c3f75c682c1c05a6e3179c518
“ShD <bolatuly.shaken@gmail.com>
2023-07-24 23:34:13 +0600

    My commit

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
-import random
-from tkinter import *
-import pandas as pnd
 from random import choice
+from tkinter import *
+
+import pandas as pnd
 
 BACKGROUND_COLOR = "#B1DDC6"
 
 # ----------------------------------- Reading CSV by Pandas ----------------------------------- #
-data_frame = pnd.read_csv("data/french_words.csv")
-data_frame_dict = pnd.DataFrame.to_dict(data_frame, orient="records")
 current_card = {}
+data_frame_dict = {}
+
+try:
+    data_frame = pnd.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pnd.read_csv("data/french_words.csv")
+    data_frame_dict = original_data.to_dict(orient="records")
+else:
+    data_frame_dict = data_frame.to_dict(orient="records")
 
 
 def french_word_random():
@@ -27,6 +34,14 @@ def card_flip():
     canvas.itemconfig(french_word, text=current_card["English"], fill="white")
     canvas.itemconfig(word_title, text="English", fill="white")
     canvas.itemconfig(card_image, image=card_back_img)
+
+
+def is_known():
+    data_frame_dict.remove(current_card)
+    data = pnd.DataFrame(data_frame_dict)
+    data.to_csv("data/words_to_learn.csv", index=False)
+
+    french_word_random()
 
 
 # ----------------------------------- UI ----------------------------------- #
@@ -52,7 +67,7 @@ canvas.grid(column=0, row=0, columnspan=2)
 
 # Buttons
 right_button_image = PhotoImage(file="images/right.png")
-right_button = Button(image=right_button_image, highlightthickness=0, border=0, command=french_word_random)
+right_button = Button(image=right_button_image, highlightthickness=0, border=0, command=is_known)
 right_button.grid(column=0, row=1)
 
 wrong_button_image = PhotoImage(file="images/wrong.png")
